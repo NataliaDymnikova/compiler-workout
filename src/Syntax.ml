562ab4c89f2c52cf369c39664a8aa3bfc6e68453
Natalia Dymnikova <natalia.dymnikova@gmail.com>
2019-02-26 14:41:24 +0300

    implement evals

--- src/Syntax.ml
+++ src/Syntax.ml
@@ -41,7 +41,30 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval s e =
+        match e with
+        | Const x -> x
+        | Var v -> s v
+        | Binop (op, x, y) ->
+            let l = eval s x in
+            let r = eval s y in
+            match op with
+            | "+" -> l + r
+            | "-" -> l - r
+            | "*" -> l * r
+            | "/" -> l / r
+            | "%" -> l mod r
+            | "!!" -> fromBool(toBool(l) || toBool(r))
+            | "&&" -> fromBool(toBool(l) && toBool(r))
+            | "==" -> fromBool(l == r)
+            | "!=" -> fromBool(l != r)
+            | "<" -> fromBool(l < r)
+            | "<=" -> fromBool(l <= r)
+            | ">" -> fromBool(l > r)
+            | ">=" -> fromBool(l >= r)
+
+        and toBool x = x!= 0
+        and fromBool x = if x then 1 else 0
 
   end
                     
@@ -65,8 +88,15 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
-                                                         
+    let eval (s, i, o) t =
+        match t with
+        | Read v -> (Expr.update v List.hd i s, List.tl i, o)
+        | Write e -> (s, i, o @ [Expr.eval s e])
+        | Assign (v, e) -> (Expr.update x (Expr.eval s e) s, i, o)
+        | Seq (e1, e2) ->
+            let (s1, i1, o1) = eval (s, i, o) e1
+            eval (s1, i1, o1) e2
+
   end
 
 (* The top-level definitions *)
